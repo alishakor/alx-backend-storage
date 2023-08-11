a28628a6320f6f7a40623aa39e384530a7ccad4b
alishakor <aliuokeowo@gmail.com>
2023-08-11 02:47:05 +0100

    modifying my comments

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,15 +4,8 @@
 -- Quantity in the table items can be negative
 
 
- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
-
--- Quantity in the table items can be negative.
-
--- Context: Updating multiple tables for one action from your application can generate issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
-
-
 DELIMITER //
-CREATE TRIGGER DecreaseItemQuantity
+CREATE TRIGGER DecreaseItemQty
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
